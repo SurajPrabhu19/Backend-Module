b54ee50bf83860a99a3419028f4e33e126ef9fff
Suraj Prabhu <surajprabhu19@gmail.com>
2023-07-13 13:35:50 +0530

    Change added - 1

---
+++ Java Basics/javabasicsp1/src/main/java/com/scaler/javabasicsp1/Javabasicsp1Application.java
@@ -0,0 +1,13 @@
+package com.scaler.javabasicsp1; // this will be the URL, e.g if www.google.com, then com.google
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javabasicsp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javabasicsp1Application.class, args);
+	}
+
+}
