fee63b26ad5c65d8bee924c684acf5bac01b87ad
Suraj Prabhu <surajprabhu19@gmail.com>
2023-07-13 13:37:17 +0530

    Changes added - 2

---
+++ Java Basics/javabasicsp1/src/test/java/com/scaler/javabasicsp1/Javabasicsp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.javabasicsp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javabasicsp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
