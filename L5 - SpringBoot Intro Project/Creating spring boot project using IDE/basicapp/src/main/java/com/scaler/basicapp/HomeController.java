e76948939bdfb68222da846b8d899f2d5d78f4c9
Suraj Prabhu <surajprabhu19@gmail.com>
2023-08-09 00:19:44 +0530

    Added Home Controller.java

---
+++ L5 - SpringBoot Intro Project/Creating spring boot project using IDE/basicapp/src/main/java/com/scaler/basicapp/HomeController.java
@@ -0,0 +1,26 @@
+package com.scaler.basicapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    // NOTE:
+    // you can name function agnostic of the route name
+    // you cannot have same routes for 2 functions
+    // you cannot have 2 func with same name - java syntax
+    @GetMapping("/hello")
+    public String helloRouteFunction() {
+        return "Routed to helloRouteFunction()";
+    }
+
+    // Dynamic Mapping:
+
+    @GetMapping("/hello/{id}")
+    public String helloRouteFunctionWithId(@PathVariable String id) {
+        return "Routed to helloRouteFunction() with Id: " + id;
+    }
+
+}
