b9f983e819ddb3dbbd92b8ae7c86ded9636c4ab3
Suraj Prabhu <surajprabhu19@gmail.com>
2023-10-30 23:27:21 +0530

    Mapped Super Class Demo

---
+++ Hibernate Inheritance/inheritancedemo/src/main/java/com/scaler/inheritancedemo/mappedsuperclass/Student.java
@@ -0,0 +1,12 @@
+package com.scaler.inheritancedemo.mappedsuperclass;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "msc_student")
+public class Student extends User {
+    private double psp;
+}
