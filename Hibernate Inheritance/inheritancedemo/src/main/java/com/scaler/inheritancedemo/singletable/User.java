d50a1fe17d4f9c49b8d333d25b26fb47b26302e8
Suraj Prabhu <surajprabhu19@gmail.com>
2023-10-30 23:27:51 +0530

    Single Table - Demo

---
+++ Hibernate Inheritance/inheritancedemo/src/main/java/com/scaler/inheritancedemo/singletable/User.java
@@ -0,0 +1,28 @@
+package com.scaler.inheritancedemo.singletable;
+
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "st_user")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "UserType", discriminatorType = DiscriminatorType.INTEGER)
+@DiscriminatorValue(value = "0")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+}
